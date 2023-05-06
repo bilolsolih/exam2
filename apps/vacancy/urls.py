655361e81d8ed835154b649ee9c7ec627e23baa2
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-05-06 17:51:46 +0500

    initial commit

--- apps/vacancy/urls.py
+++ apps/vacancy/urls.py
@@ -7,5 +7,4 @@ app_name = 'vacancy'
 urlpatterns = [
     path('vacancy/', VacancyListAPIView.as_view(), name='vacancy_list'),
     path('vacancy/<int:salary>/', VacancyListAPIView.as_view(), name='vacancy_list_by_salary'),
-
 ]
