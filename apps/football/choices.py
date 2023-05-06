54cf1b941618f96b53cbb6e999b52f1855486c55
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-05-06 17:40:49 +0500

    initial commit

---
+++ apps/football/choices.py
@@ -0,0 +1,28 @@
+PLAYER_POSITION = (
+    ('gk', 'Goal keeper'),
+    ('df', 'Defender'),
+    ('md', 'Middle player'),
+    ('at', 'Attacker')
+)
+REFEREE_POSITION = (
+    ('mn', 'Main'),
+    ('sd', 'Side'),
+    ('tb', 'Tablet shower')
+)
+
+GOAL_TYPE = (
+    ('nm', 'Normal'),
+    ('pn', 'Penalty'),
+    ('cn', 'From the corner'),
+    ('ag', 'Auto goal')
+)
+CARD_TYPE = (
+    ('r', 'Red'),
+    ('y', 'Yellow')
+)
+CARD_REASONS = (
+    ('hd', 'Holding'),
+    ('rg', 'Roughing'),
+    ('fg', 'Fighting'),
+    ('tr', 'Tripping')
+)
--- apps/football/models.py
+++ apps/football/models.py
@@ -1,5 +1,8 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils import timezone
+
+from .choices import PLAYER_POSITION, REFEREE_POSITION, GOAL_TYPE, CARD_TYPE, CARD_REASONS
 
 
 class Country(models.Model):
@@ -25,9 +28,10 @@ class Match(models.Model):
     guest = models.ForeignKey('football.Team', related_name='matches_as_guest', on_delete=models.SET_NULL, null=True)
     host_goals = models.PositiveIntegerField(default=0)
     guest_goals = models.PositiveIntegerField(default=0)
-
     date = models.DateTimeField(default=timezone.now)
-
+    match_order_number = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(2)])
+    is_finished = models.BooleanField(default=True)
+    referee = None
 
     class Meta:
         verbose_name = 'Match'
@@ -46,6 +50,59 @@ class Match(models.Model):
         return f'Match between {self.host} and {self.guest}'
 
 
+class Referee(models.Model):
+    first_name = models.CharField(max_length=128)
+    middle_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    position = models.CharField(choices=REFEREE_POSITION, max_length=2)
+
+    class Meta:
+        verbose_name = 'Referee'
+        verbose_name_plural = 'Referees'
+
+
 class Team(models.Model):
     title = models.CharField(max_length=128)
-    player = None
+    flag = models.ImageField(upload_to='football/teams/flags/')
+    league = models.ForeignKey(to='football.League', related_name='teams', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=128)
+    middle_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    player_number = models.PositiveIntegerField(default=0)
+    position = models.CharField(choices=PLAYER_POSITION, max_length=2)
+    team = models.ForeignKey(to='football.League', related_name='players', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = 'Player'
+        verbose_name_plural = 'Players'
+
+
+class Goal(models.Model):
+    match = models.ForeignKey(to='football.Match', related_name='goals', on_delete=models.CASCADE)
+    player = models.ForeignKey(to='football.Player', related_name='goals', on_delete=models.CASCADE)
+    type = models.CharField(choices=GOAL_TYPE, max_length=2, default='nm')
+    scored_minute = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Goal'
+        verbose_name_plural = 'Goals'
+
+
+class Card(models.Model):
+    type = models.CharField(choices=CARD_TYPE, max_length=1)
+    referee = models.ForeignKey(to='football.Referee', related_name='cards', on_delete=models.SET_NULL, null=True)
+    match = models.ForeignKey(to='football.Match', related_name='cards', on_delete=models.CASCADE)
+    player = models.ForeignKey(to='football.Player', related_name='cards', on_delete=models.CASCADE)
+    reason = models.CharField(choices=CARD_REASONS, max_length=2)
+    given_minute = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Card'
+        verbose_name_plural = 'Cards'
